df7fbedba937bbc1c5c6c6b6d6c910d5f51f9c29
suman <suman143learning@gmail.com>
2019-02-04 19:54:13 +0530

    project

---
+++ AAA/src/test/java/pack1/TestSuite1.java
@@ -0,0 +1,15 @@
+package pack1;
+
+import org.junit.Test;
+
+public class TestSuite1 {
+
+	@Test
+	public void test() {
+		String path=System.getProperty("user.dir");
+		System.out.println(path);
+		
+	
+	}
+
+}
